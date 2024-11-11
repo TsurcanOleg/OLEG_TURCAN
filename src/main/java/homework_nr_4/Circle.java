2f59a80648053da88c53d7bf668f9b112750a3b0
OLEG_TURCAN <tsurcan.oleg@gmail.com>
2024-11-11 22:10:51 +0200

    Homework4, calculate area

--- src/main/java/homework_nr_4/Circle.java
+++ src/main/java/homework_nr_4/Circle.java
@@ -4,5 +4,16 @@ public class Circle {
 
     public static void main(String[] args) {
 
+        float radius = 6F;
+        float Area = calculateArea(radius);
+
+        System.out.println(Area);
+
     }
+
+    public static float calculateArea (float radius ){
+        float areaCircle = 3.14F * radius * radius ;
+        return areaCircle;
+    }
+
 }
