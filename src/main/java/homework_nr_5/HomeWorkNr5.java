12987c5145b6b9404d058232ee8e9266b79190a3
OLEG_TURCAN <tsurcan.oleg@gmail.com>
2024-11-16 12:29:22 +0200

    Homework5, change array as per comment

--- src/main/java/homework_nr_5/HomeWorkNr5.java
+++ src/main/java/homework_nr_5/HomeWorkNr5.java
@@ -6,25 +6,12 @@ public class HomeWorkNr5 {
 
     public static void main(String[] args) {
 
-        int[] number = new int[]{
-                0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-                10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-                20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-                30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
-                40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
-                50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
-                60, 61, 62, 63, 64, 65, 66, 67, 68, 69,
-                70, 71, 72, 73, 74, 75, 76, 77, 78, 79,
-                80, 81, 82, 83, 84, 85, 86, 87, 88, 89,
-                90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100
-
+        int[] number = new int[101];
+        for (int i = 0; i < 101; i++){
+            number[i] = i;
+            System.out.println(number[i]);
         };
 
-        for (int i = 0; i < 101; i++) {
-            System.out.println(number[i]);
-        }
-
-        System.out.println("Summ array number");
         // find summ array number
 
         int sumEven = 0;
@@ -33,7 +20,7 @@ public class HomeWorkNr5 {
                 sumEven += number[j];
             }
         }
-        System.out.println(sumEven);
+        System.out.println("Sum Even = " + sumEven);
 
         long multiplyOdd = 1;
         for (int k = 0; k < number.length; k++) {
@@ -41,7 +28,7 @@ public class HomeWorkNr5 {
                 multiplyOdd *= number[k];
             }
         }
-        System.out.println(multiplyOdd);
+        System.out.println("Multiply Odd = " + multiplyOdd);
 
         int[] copyNumbers = new int[number.length];
         for (int i = 0; i < number.length; i++) {
