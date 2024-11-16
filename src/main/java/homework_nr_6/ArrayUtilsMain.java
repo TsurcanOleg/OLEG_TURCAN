3fc7883b3f63d77fa6216d4c0f1086139a620203
OLEG_TURCAN <tsurcan.oleg@gmail.com>
2024-11-16 14:27:17 +0200

    Homework6, ArraUntils MaxMin

--- src/main/java/homework_nr_6/ArrayUtilsMain.java
+++ src/main/java/homework_nr_6/ArrayUtilsMain.java
@@ -4,8 +4,8 @@ public class ArrayUtilsMain {
 
     public static void main(String[] args) {
 
-        System.out.println(ArrayUtils.findMax(3, 5, 6, 89));
-        System.out.println(ArrayUtils.findMin(54, 64, 78, 9));
+        System.out.println(ArrayUtils.findMax(3, 5, 6, 89, 105, -9, 16));
+        System.out.println(ArrayUtils.findMin(54, 64, 78, 9, -5, 150, -342));
 
     }
 
