f8d6ede84b1b46846a61f0cb53b47e17ddf2223b
OLEG_TURCAN <tsurcan.oleg@gmail.com>
2024-11-16 14:25:47 +0200

    Homework5, change array as per comment

---
+++ src/main/java/homework_nr_6/ArrayUtilsMain.java
@@ -0,0 +1,13 @@
+package homework_nr_6;
+
+public class ArrayUtilsMain {
+
+    public static void main(String[] args) {
+
+        System.out.println(ArrayUtils.findMax(3, 5, 6, 89));
+        System.out.println(ArrayUtils.findMin(54, 64, 78, 9));
+
+    }
+
+
+}
