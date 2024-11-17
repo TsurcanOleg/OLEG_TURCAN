0336e96890ed9b662704f7180e843378e58d66ae
OLEG_TURCAN <tsurcan.oleg@gmail.com>
2024-11-17 22:02:04 +0200

    Homework6, BankAccound

---
+++ src/main/java/homework_nr_6/NumberUtils.java
@@ -0,0 +1,28 @@
+package homework_nr_6;
+
+public class NumberUtils {
+
+    public static void main(String[] args) {
+    }
+
+    public static boolean isPrime(int number) {
+        if (number < 2) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int findPrimesInArray(int... numbers) {
+        for (int i = 1; i < numbers.length; i++) {
+            if (isPrime(numbers[i])) {
+            }
+            return numbers[i];
+        }
+        return 0;
+    }
+}
