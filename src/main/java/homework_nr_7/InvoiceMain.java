f945fe8eeac0d0780287eca0f88eb87f4b80d690
OLEG_TURCAN <tsurcan.oleg@gmail.com>
2024-11-18 00:28:54 +0200

    Homework7, create invice

---
+++ src/main/java/homework_nr_7/InvoiceMain.java
@@ -0,0 +1,11 @@
+package homework_nr_7;
+
+public class InvoiceMain {
+    public static void main(String[] args) {
+
+        Invoice inv1 = new Invoice("Samsung", "Phone", 4, 1200);
+        Invoice inv2 = new Invoice("Samsung", "Tablet", 2, 2200);
+        Invoice inv3 = new Invoice("Toshiba", "Laptop", 1, 16000);
+        Invoice inv4 = new Invoice("Karcher", "Vacuum cleaner", 2, 15400);
+    }
+}
