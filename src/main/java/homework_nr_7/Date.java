25d9e58410d9cbf5c2faf2d4dc66b4654adb21ae
OLEG_TURCAN <tsurcan.oleg@gmail.com>
2024-11-19 22:02:08 +0200

    Homework7, create class Date

---
+++ src/main/java/homework_nr_7/Date.java
@@ -0,0 +1,14 @@
+package homework_nr_7;
+
+public class Date {
+
+    int day;
+    String Month;
+    int year;
+
+    public Date(int day, String month, int year) {
+        this.day = day;
+        Month = month;
+        this.year = year;
+    }
+}
