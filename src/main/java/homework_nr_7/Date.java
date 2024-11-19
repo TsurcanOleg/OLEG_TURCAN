34d842aaa6ef60f5d748d7b0b658667d3a43e632
OLEG_TURCAN <tsurcan.oleg@gmail.com>
2024-11-20 00:16:46 +0200

    Homework7, class Date Encapsulation

--- src/main/java/homework_nr_7/Date.java
+++ src/main/java/homework_nr_7/Date.java
@@ -23,17 +23,38 @@ public class Date {
         this.month = month;
         this.year = year;
     }
+
     public int displayDate() {
-        int dateToShow = 0;
         if (1 > month) {
-            return 1;
+            return 0;
         } else if (12 < month) {
-            return 2;
+            return 0;
+        }
+        switch (month) {
+            case 1, 3, 5, 7, 8, 10, 12:
+                if (1 > day) {
+                    return 0;
+                } else if (31 < day) {
+                    return 0;
+                }
+                break;
+            case 2:
+                if (1 > day) {
+                    return 0;
+                } else if (28 < day) {
+                    return 0;
+                }
+                break;
+            case 4, 6, 9, 11:
+                if (1 > day) {
+                    return 0;
+                } else if (30 < day) {
+                    return 0;
+                }
+                break;
+            default:
         }
         System.out.println(day + "/" + month + "/" + year);
-        return dateToShow;
+        return 0;
     }
 }
-
-
-
--- src/main/java/homework_nr_7/EncapsulationDateMain.java
+++ src/main/java/homework_nr_7/EncapsulationDateMain.java
@@ -3,10 +3,17 @@ package homework_nr_7;
 public class EncapsulationDateMain {
     public static void main(String[] args) {
 
-        Date d1 = new Date(21, 8,2023);
+        Date d1 = new Date(12, 8,2023);
+        Date d2 = new Date(21, 12,2023);
+        Date d3 = new Date(32, 8,2023);
+        Date d4 = new Date(12, 13,2023);
+
 
 
         System.out.println(d1.displayDate());
+        System.out.println(d2.displayDate());
+        System.out.println(d3.displayDate());
+        System.out.println(d4.displayDate());
     }
 
 }
