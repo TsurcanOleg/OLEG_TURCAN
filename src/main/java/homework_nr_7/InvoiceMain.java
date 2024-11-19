ce94cf4b261db1c24a33d73fe7d6cbd69e7bb9a4
OLEG_TURCAN <tsurcan.oleg@gmail.com>
2024-11-19 21:58:33 +0200

    Homework7, Invoice

--- src/main/java/homework_nr_7/InvoiceMain.java
+++ src/main/java/homework_nr_7/InvoiceMain.java
@@ -1,18 +1,25 @@
 package homework_nr_7;
 
+import java.sql.SQLOutput;
+
 public class InvoiceMain {
 
     public static void main(String[] args) {
 
-        ElectricalAppliance ea1 = new ElectricalAppliance("Smasung", "S9", 2, 15000D);
-        ElectricalAppliance ea2 = new ElectricalAppliance("Toshiba", "Laptop", 1, 19000D);
-        ElectricalAppliance ea3 = new ElectricalAppliance("LG", "Vacuum", 2, 8500D);
-        ElectricalAppliance ea4 = new ElectricalAppliance("JBL", "box", 5, 3000D);
+        Invoice in1 = new Invoice("Samsung", "Phone", 1, 12300D);
+        Invoice in2 = new Invoice("Toshiba", "Laptop", 6, 12499D);
+        Invoice in3 = new Invoice("LG", "TV", 7, 55040D);
+        Invoice in4 = new Invoice("JBL", "Box", 5, 3690D);
 
-        Invoice inv1 = new Invoice();
+        InvoiceOrder io1 = new InvoiceOrder(in1, in2, in3, in4);
+        InvoiceOrder io2 = new InvoiceOrder(in1, in3, in4);
 
 
-
-
+        double sumInvoice = 0;
+        for (int i = 0; i != io1.invoices.length; i++) {
+            sumInvoice += io1.invoices[i].price * io1.invoices[i].quantity;
+        }
+        System.out.println(sumInvoice);
+        System.out.println(io2.getInvoiceTotal());
     }
 }
