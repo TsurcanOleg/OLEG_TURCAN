7f2a590174a19b0fa9f6a2971c773ccc1638bb7e
OLEG_TURCAN <tsurcan.oleg@gmail.com>
2024-11-20 22:59:40 +0200

    Homework7, ex8

--- src/main/java/homework_nr_7/Date.java
+++ src/main/java/homework_nr_7/Date.java
@@ -22,32 +22,13 @@ public class Date {
         this.day = day;
         this.month = month;
         this.year = year;
-
-        if (month < 1) {
-        } else if (month > 12) {
-        }
-        switch (month) {
-            case 1, 3, 5, 7, 8, 10, 12:
-                if (day < 1) {
-                } else if (day > 31) {
-                }
-                break;
-            case 2:
-                if (day < 1) {
-                } else if (day > 28) {
-                }
-                break;
-            case 4, 6, 9, 11:
-                if (day < 1) {
-                } else if (day > 30) {
-                }
-                break;
-            default:
-        }
     }
 
     public void displayDate() {
-        System.out.println(day +"/"+ month +"/"+year);
+        if (this.day < 1 || this.day > 31 || this.month < 1 || this.month > 12 || this.year <= 0) {
+            System.out.println("Invalid data!");
+        } else {
+            System.out.println(day + "/" + month + "/" + year);
+        }
+    }
 }
-}
-
--- src/main/java/homework_nr_7/EncapsulationDateMain.java
+++ src/main/java/homework_nr_7/EncapsulationDateMain.java
@@ -3,10 +3,16 @@ package homework_nr_7;
 public class EncapsulationDateMain {
     public static void main(String[] args) {
 
-        Date d1 = new Date(12, 8, 2023);
+        Date d1 = new Date(12, 12, 2023);
         Date d2 = new Date(21, 12, 2025);
-        Date d3 = new Date(31, 8, 2033);
-        Date d4 = new Date(12, 13, 2015);
+        Date d3 = new Date(32, 8, 2033);
+        Date d4 = new Date(13, 13, 2015);
+
+
+        d1.displayDate();
+        d2.displayDate();
+        d3.displayDate();
+        d4.displayDate();
 
 
     }
