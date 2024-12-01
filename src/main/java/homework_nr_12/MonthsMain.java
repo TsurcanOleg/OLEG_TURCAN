a9f798a57a841744745d96f8b7a0368eda187433
OLEG_TURCAN <tsurcan.oleg@gmail.com>
2024-12-01 23:27:23 +0200

    Homework12, Months

---
+++ src/main/java/homework_nr_12/MonthsMain.java
@@ -0,0 +1,17 @@
+package homework_nr_12;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MonthsMain {
+    public static void main(String[] args) {
+
+        Months m1 = Months.DECEMBER;
+        System.out.println(m1);
+
+        System.out.println("--------------");
+       for (Months months : Months.values()){
+           System.out.println(months);
+       }
+    }
+}
