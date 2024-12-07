489c03195e74324c6bf2c2c75fdbae4dbb9de801
OLEG_TURCAN <tsurcan.oleg@gmail.com>
2024-12-08 01:09:53 +0200

    Homework14, Create AirPlane

---
+++ src/main/java/homewrok_nr_14/AirPlane.java
@@ -0,0 +1,39 @@
+package homewrok_nr_14;
+
+public class AirPlane {
+
+
+    public static void main(String[] args) {
+
+        createAirplaceLBYL("Steel", "Deluxe", 6);
+        createAirplaceLBYL("", "777", 4);
+
+
+
+
+    }
+    public static void createAirplaceLBYL (String manufacturer, String model, int nrOfEngine) {
+        if (nrOfEngine == 0 || nrOfEngine % 2 != 0) {
+            System.out.println("Number of engines is zero or odd!!!");
+            try {
+                throw new InvalidNrOfEnginesException();
+            }catch (InvalidNrOfEnginesException e){
+                e.printStackTrace();
+            }
+        }
+        if (manufacturer.isEmpty()){
+        try {
+            throw new InvalidManufacturerException("Invalid or not found manufacturer");
+        }catch (InvalidManufacturerException e) {
+            e.printStackTrace();
+        }
+        }
+        if (model.isEmpty()){
+            try{
+                throw new InvalidModelException("Invalid or not found model");
+            }catch (InvalidModelException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
