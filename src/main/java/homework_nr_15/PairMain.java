25a30e62e93c3ac7c8db29838586952c0bc9a191
OLEG_TURCAN <tsurcan.oleg@gmail.com>
2024-12-08 18:22:15 +0200

    Homework15, Pair & Box

--- src/main/java/homework_nr_15/PairMain.java
+++ src/main/java/homework_nr_15/PairMain.java
@@ -3,8 +3,8 @@ package homework_nr_15;
 public class PairMain {
     public static void main(String[] args) {
 
-        Pair p1 = new Pair("Hellor", 42);
-        Pair p2 = new Pair("Java", true);
+        Pair <String, Integer> p1 = new Pair<>("Hello", 42);
+        Pair <String, Boolean> p2 = new Pair<>("Java", true);
 
 
 
