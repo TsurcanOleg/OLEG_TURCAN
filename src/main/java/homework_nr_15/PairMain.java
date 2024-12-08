88106dff8c9ee01ad11ec8c336f7eb0fdfae027b
OLEG_TURCAN <tsurcan.oleg@gmail.com>
2024-12-08 18:15:15 +0200

    Homework15, Pair

---
+++ src/main/java/homework_nr_15/PairMain.java
@@ -0,0 +1,17 @@
+package homework_nr_15;
+
+public class PairMain {
+    public static void main(String[] args) {
+
+        Pair p1 = new Pair("Hellor", 42);
+        Pair p2 = new Pair("Java", true);
+
+
+
+        System.out.println(p1.getFirst());
+        System.out.println(p1.getSecond());
+
+        System.out.println(p2.getSecond());
+
+    }
+}
